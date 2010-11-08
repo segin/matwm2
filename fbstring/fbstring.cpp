9607d880b1a0177faefaa35149b48b2b79d07784
segin2005@gmail.com <segin2005@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2010-11-08 17:57:29 +0000

    Implemented a few of the new constructors, new helper functions, expanded class definition

--- fbstring/fbstring.cpp
+++ fbstring/fbstring.cpp
@@ -1,85 +1,148 @@
+/* FreeBASIC FBSTRING implementation in C++ */
+
 #include <string>
 
-
-class FBSTRING { 
-private:
-	char *sz_data;
-	size_t sz_len;
-	size_t sz_alloc;
-public:
-	FBSTRING();
-	FBSTRING(const FBSTRING& str)
-	FBSTRING(const FBSTRING& str, size_t pos, size_t n = npos);
-
-}
-
-/* from yetisoft's tinybasic */
+/* from yetifoot's tinybasic */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-struct FBSTRING {
+
+class FBSTRING {
+public:
 	FBSTRING();
 	FBSTRING(const char *);
-	FBSTRING(const FBSTRING&);
+	FBSTRING(const char *, const size_t);
+	FBSTRING(const char *, const size_t, const size_t);
+	FBSTRING(const std::string &);
+	FBSTRING(const std::string &, const size_t);
+	FBSTRING(const std::string &, const size_t, const size_t);
+	FBSTRING(const FBSTRING &);
+	FBSTRING(const FBSTRING &, const size_t);
+	FBSTRING(const FBSTRING &, const size_t, const size_t);
 	~FBSTRING();
-	int operator=(const char*);
-	int operator=(const FBSTRING&);
-	int operator==(const FBSTRING&);
-	int operator!=(const FBSTRING&);
-	int operator+=(const FBSTRING&);
+	int operator=(const int);
+	int operator=(const short);
+	int operator=(const float);
+	int operator=(const double);
+	int operator=(const char);	
+	int operator=(const char *);
+	int operator=(const std::string &);
+	int operator=(const FBSTRING &);
+	int operator==(const FBSTRING &);
+	int operator!=(const FBSTRING &);
+	int operator+=(const FBSTRING &);
+private:
 	void *data;
 	int len;
 	int size;
+	void init_FBSTRING(void *, int); /* common constructor bits */
+	void fini_FBSTRING(); /* common destructor bits */
 };
+
 typedef int integer;
 typedef char zstring;
+
 void print(const FBSTRING&, integer nl);
+
 FBSTRING& concat(const FBSTRING&, const FBSTRING&);
 FBSTRING& lcase(const FBSTRING&);
 FBSTRING& int_to_str(integer);
 FBSTRING& chr(integer);
 integer asc(const FBSTRING&);
 integer val(const FBSTRING&);
+
 #define exit_ exit
 #define EOF_ EOF
 #define callocate(n) calloc(n, 1 )
+
+/* perform allocations in 4k chunks; as FreeBASIC only runs on x86, this
+ * allows for (hopefully) efficent memory allocation since we're allocating
+ * an entire page at once.
+ */
+
+void FBSTRING::init_FBSTRING(void *str, int length);
+{
+	int m;
+	m = length / 4096;
+	if((length % 4096) > 0) 
+		m++;
+	size = m * 4096;	
+	data = ::malloc(size);
+	len = length;
+	::memcpy(data, str, length);
+	(char *) data[length] = 0;
+}
+
+void FBSTRING::fini_FBSTRING()
+{
+	if(data) free(data);
+	data = 0;
+	len = 0;
+	size = 0;
+}
+
+/* initialize an empty string */
 FBSTRING::FBSTRING()
 {
 	data = 0;
 	len = 0;
 	size = 0;
 }
+
+/* initialize a string from a character array */
 FBSTRING::FBSTRING(const char *s)
 {
 	data = 0;
 	len = 0;
 	size = 0;
-	if(s == 0) return;
+	if((int) s == 0) return;
 	int s_len = strlen(s);
-	data = malloc(s_len);
-	len = s_len;
-	size = s_len;
-	memcpy(data, s, s_len);
+	init_FBSTRING((void *) s, s_len);
 }
+
+/* initialize a string from a character array, with a given starting offset. */
+FBSTRING::FBSTRING(const char *s, const size_t l)
+{
+	data = 0;
+	len = 0;
+	size = 0;
+	if((int) s == 0) return;
+	int s_len = strlen(s);
+	if (s_len < (int) l) return;
+	int m = s_len - (int) l;
+	init_FBSTRING((void *) s + (void *) l, m);
+}
+
+/* initialize a string from a character array, with a given starting offset
+ * plus max length */
+FBSTRING::FBSTRING(const char *s, const size_t l, const size_t x)
+{
+	data = 0;
+	len = 0;
+	size = 0;
+	if(((int) s == 0) || ((int) x == 0)) return;
+	int s_len = strlen(s);
+	if (s_len < (int) l) return;
+	int m = s_len - (int) l;
+	init_FBSTRING((void *) ((int) s + (int) l, (int) (m < x ? m : x));
+}
+
 FBSTRING::FBSTRING(const FBSTRING& s)
 {
 	data = 0;
 	len = 0;
 	size = 0;
 	if(s.data == 0) return;
-	data = malloc(s.len);
-	len = s.len;
-	size = s.len;
-	memcpy(data, s.data, s.len);
+	init_FBSTRING(s, s_len);
 }
+
 FBSTRING::~FBSTRING()
 {
-	data = 0;
-	len = 0;
-	size = 0;
+	fini_FBSTRING();
 }
+
 int FBSTRING::operator=(const char *s)
 {
 	if(data) free(data);
@@ -91,9 +154,10 @@ int FBSTRING::operator=(const char *s)
 	data = malloc(s_len);
 	len = s_len;
 	size = s_len;
-	memcpy(data, s, s_len);
+	::memcpy(data, s, s_len);
 	return 0;
 }
+
 int FBSTRING::operator=(const FBSTRING& s)
 {
 	data = 0;
@@ -106,6 +170,7 @@ int FBSTRING::operator=(const FBSTRING& s)
 	memcpy(data, s.data, s.len);
 	return 0;
 }
+
 int FBSTRING::operator==(const FBSTRING& s)
 {
 	if(len != s.len) return 0;
@@ -114,6 +179,7 @@ int FBSTRING::operator==(const FBSTRING& s)
 	if(memcmp(data, s.data, len) != 0) return 0;
 	return 1;
 }
+
 int FBSTRING::operator!=(const FBSTRING& s)
 {
 	if(len != s.len) return 1;
@@ -122,6 +188,7 @@ int FBSTRING::operator!=(const FBSTRING& s)
 	if(memcmp(data, s.data, len) != 0) return 1;
 	return 0;
 }
+
 int FBSTRING::operator+=(const FBSTRING& s)
 {
 	FBSTRING result;
@@ -135,6 +202,9 @@ int FBSTRING::operator+=(const FBSTRING& s)
 	size = result.size;
 	return 0;
 }
+
+/*
+
 void print(const FBSTRING& s, integer nl)
 {
 	int pos;
@@ -143,6 +213,7 @@ void print(const FBSTRING& s, integer nl)
 	}
 	if(nl) fputc(10, stdout);
 }
+
 FBSTRING& concat(const FBSTRING& s1, const FBSTRING& s2)
 {
 	FBSTRING *result = (FBSTRING *)calloc(sizeof(FBSTRING), 1);
@@ -153,6 +224,7 @@ FBSTRING& concat(const FBSTRING& s1, const FBSTRING& s2)
 	memcpy(&(((char *)result->data)[s1.len]), s2.data, s2.len);
 	return *result;
 }
+
 FBSTRING& lcase(const FBSTRING& s)
 {
 	FBSTRING *result = (FBSTRING *)calloc(sizeof(FBSTRING), 1);
@@ -163,12 +235,14 @@ FBSTRING& lcase(const FBSTRING& s)
 	}
 	return *result;
 }
+
 FBSTRING& str_temp(const FBSTRING& s)
 {
 	FBSTRING *result = (FBSTRING *)calloc(sizeof(FBSTRING), 1);
 	*result = s;
 	return *result;
 }
+
 FBSTRING& int_to_str(integer i)
 {
 	FBSTRING *result = (FBSTRING *)calloc(sizeof(FBSTRING), 1);
@@ -180,6 +254,7 @@ FBSTRING& int_to_str(integer i)
 	result->size = 32;
 	return *result;
 }
+
 FBSTRING& chr(integer i)
 {
 	FBSTRING *result = (FBSTRING *)calloc(sizeof(FBSTRING), 1);
@@ -189,6 +264,7 @@ FBSTRING& chr(integer i)
 	((char *)result->data)[0] = i;
 	return *result;
 }
+
 integer val(const FBSTRING& s)
 {
 	int n;
@@ -198,8 +274,10 @@ integer val(const FBSTRING& s)
 	free(tmp);
 	return n;
 }
+
 integer asc(const FBSTRING& s)
 {
 	if(s.data == 0) return -1;
 	return ((char *)s.data)[0];
 }
+*/
