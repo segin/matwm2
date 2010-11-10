4521de3afef6ba5a36e7b93c7197b82992f7361e
segin2005 <segin2005@8792b224-6933-e0e1-cdff-19ba7c209891>
2010-11-10 23:07:28 +0000

    More code improvements, more implemented

--- md5sum/main.c
+++ md5sum/main.c
@@ -5,8 +5,9 @@
 #include <getopt.h>
 #include "md5.h"
 
-void usage(char *argv0);
+void usage(const char *argv0);
 void version(void);
+int do_md5sum(const char *file, md5_byte_t *sum);
 
 static int flags[8];
 
@@ -22,7 +23,7 @@ enum {
 };
 
 enum {
-	MODE_TEXT = 0,
+	MODE_TEXT = 1,
 	MODE_BINARY
 };
 
@@ -31,7 +32,19 @@ void version(void)
 	puts("md5sum version 0.1");
 }
 
-void usage(char *argv0)
+int do_md5sum(const char *file, md5_byte_t *sum)
+{
+	FILE *fd;
+	char *md;
+	if(flags[FLAG_FILEMODE] == MODE_BINARY) {
+		md = "bt";
+	} else {
+		md = "rt";
+	}
+	/* if((fd = fopen( */
+}
+
+void usage(const char *argv0)
 {
 	printf("usage: %s [OPTION]... [FILE]...\n", argv0);
 	printf("\nWhen FILE is - or not specified, read standard input.\n");
@@ -43,7 +56,7 @@ void usage(char *argv0)
 	       "\t    --quiet\tdon't print OK for each file passing as valid\n"
 	       "\t    --status\tdon't output anything, status codes show success\n"
 	       "\t-w, --warn\twarn about invalid formatting in listfiles\n\n"
-	       "\t    --help\tPrints usage and quits."
+	       "\t    --help\tPrints usage and quits.\n"
 	       "\t    --version\t Prints version and quits.\n\n"
 	       "The -c, --check flag is unimplemented.\n"
 	       "Send bug reports to <segin2005@gmail.com>.\n");
@@ -55,8 +68,7 @@ int main(int argc, char *argv[])
 	md5_state_t state;
 	int ch;
 	md5_byte_t sum[16];
-	char *rmode;
-	FILE *list, *file;
+
 	static struct option longopts[] = {
 		{ "text",	no_argument,	0,	't' },
 		{ "binary",	no_argument,	0,	'b' },
@@ -74,7 +86,12 @@ int main(int argc, char *argv[])
 	while((ch = getopt_long(argc, argv, "bctw", longopts, NULL)) != -1) {
 		switch(ch) { 
 			case 't':
-				flags[FLAG_FILEMODE] = MODE_TEXT;
+				if(flags[FLAG_FILEMODE] == 0) {
+					flags[FLAG_FILEMODE] = MODE_TEXT;
+				} else {
+					fprintf(stderr, "%s: --binary and "
+						"--text are mutually "
+						"exclusive.\n", argv[0]); 
 				break;
 			case 'b':
 				flags[FLAG_FILEMODE] = MODE_BINARY;
@@ -105,13 +122,18 @@ int main(int argc, char *argv[])
 				break; 
 		};
 	};
+	if((flags[FLAG_FILEMODE] > 0) && (flags[FLAG_CHECK] > 0)) {
+		fprintf(stderr, "%s: --binary and --text are "
+				"meaningless with --check.\n", argv[0]);
+		exit(2);
+	}
 	for(ch = 0; ch < 8; ch++) printf("%d ", flags[ch]);
 	putchar('\n');
 	while(optind < argc) {
 		if(flags[FLAG_CHECK] == 1) {
 			/* check_list(argv[optind++]); */
 		} else {
-			do_md5sum(argv[optind++]);
+			do_md5sum(argv[optind++], sum);
 		}
 	};
 	return 0;	
