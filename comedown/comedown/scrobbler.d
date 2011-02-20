9611134eb61e47d872a88f6b3cfe184286fd7ae7
segin2005 <segin2005@8792b224-6933-e0e1-cdff-19ba7c209891>
2011-02-20 03:27:23 +0000

    Imported D language Last.fm scrobbler lib, "Comedown".

--- comedown/comedown/scrobbler.d
+++ comedown/comedown/scrobbler.d
@@ -1,6 +1,8 @@
 /* scrobbler.d: Main scrobbler class of Comedown 
  *
  * Copyright (c) 2010 Kirn Gill <segin2005@gmail.com>
+ * 
+ * Portions based on HTMLget example by Christopher E. Miller
  *  
  * Permission is hereby granted, free of charge, to any person or organization
  * obtaining a copy of the software and accompanying documentation covered by
@@ -31,37 +33,132 @@ import comedown.types.scrobble;
 
 import std.array;
 import std.string;
-// import std.xml;
+import std.xml;
 import std.md5;
+import std.uri;
+import std.conv;
+import std.date;
+import std.stdio;
+import std.string;
+import std.socket;
+import std.socketstream;
+import std.c.stdio;
+import std.c.string;
 
-private import std.stdio;
-private import std.string;
-private import std.c.stdio;
-private import std.c.string;
+struct APIPath {
 
-struct APIPath { 
+public:
 	string host;
-	int port;
+	ushort port;
 	string path;
+
+	this(string host, ushort port, string path)
+	{
+		this.host = host;
+		this.port = port;
+		this.path = path;
+	}
 };
 
 class Scrobbler {
 
 private:
 	Scrobble[] scrobbles;
+	string appid;
+	string appver;
 	string user;
 	string pass;
+	string status;
 	string sessionkey;
+	string server;
 	APIPath npPath;
 	APIPath submitPath;		
 
-public: 
-	this(string user, string pass)
+	void init() 
 	{
+		sessionkey = getSessionKey();
+	}
+
+	/* This function also sets the path for "now playing" and 
+	 * "scrobble" submits. 
+	 */
+	string getSessionKey()
+	{
+		writefln("comedown.scrobbler: Last.fm username: " ~ 
+			user ~ ", password: " ~ "*".repeat(pass.length));
+		auto curtime = to!string(getUTCtime() / ticksPerSecond);
+		auto authkey = MD5Str(MD5Str(pass) ~ curtime); 
+		writefln("curtime: " ~ curtime ~ ", authkey: " ~ authkey);
+		auto a = APIPath(, 80, "/");
+		auto getdata = [ 
+				"hs" : "true", 
+				"p"  : "1.2.1",
+				"c"  : appid,
+				"v"  : appver,
+				"u"  : user,
+				"t"  : curtime,
+				"a"  : authkey
+			    ];
+		auto response = submitData(a, getdata, "GET");
+		if(response.length == 0) {
+			return "";
+		} else {
+			;
+		}
+		return "";
+	}
+
+	string[] submitData(APIPath path, string[string] request, string requestType)
+	{
+		string[] returnData;
+		auto requestSerial = requestType == "GET" ? "?" : "";
+		foreach(requestKey, requestValue; request) {
+			requestSerial ~= encodeComponent(requestKey) ~ "=" ~
+				encodeComponent(requestValue) ~ "&";
+		};
+
+		requestSerial.popBack;
+
+		writefln(requestSerial);
+		
+		try { 
+			auto sock = new TcpSocket(new InternetAddress
+				(path.host, cast(ushort) path.port));
+			auto ss = new SocketStream(sock);
+		} 
+
+		catch (Exception e) {
+			std.stdio.stderr.writefln("Exception: " ~ e.toString());
+		}
+		
+		return [];
+	}
+
+	string MD5Str(string str)
+	{	
+		ubyte[16u] digest;
+		MD5_CTX context;
+		context.start();
+		context.update(str);
+		context.finish(digest);
+		return digestToString(digest);
+	}
+
+public: 
+	this(string server, string appid, string appver, string user, string pass)
+	{
+		this.server = server;
+		this.appid = appid;
+		this.user = user;
+		this.pass = pass;
+		init();
+	}
+
+	void setUser(string user, string pass)
+	{
+		sessionkey = "";
 		this.user = user;
 		this.pass = pass;
 	}
-
-	
 
 }
--- comedown/test.d
+++ comedown/test.d
@@ -6,19 +6,33 @@ import std.uri;
 import std.string;
 import std.file;
 
-// import comedown.scrobbler;
+import comedown.scrobbler;
 
 void main(string[] args) 
 {
 	try { 
 		string config = cast(string) std.file.read("lastfm_config.txt");
+		auto lines = std.string.splitlines(config);
+		if(lines.length < 2) { 
+			writefln("Config file contains too few lines!");
+			return;
+		}
+		
+		for(int i = 0; i < lines.length ; i++) { 
+			lines[i] = lines[i].strip();
+		}
+
+		auto scrobbler = new Scrobbler("tst", "1.0", lines[0], lines[1]);
+
+		
+			
 	} 
 
 	catch(FileException e) {
 		writefln("Cannot open config file: " ~ e.toString());
 		return;	
 	}
-	
+
 	writefln("Hello World!");
 	//auto encoded = encodeComponent(orig);
 	//writefln("Orginal: %s, Encoded: %s", orig, encoded);
