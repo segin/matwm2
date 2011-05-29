009c18b40517bbdca206638e796a10b8f5ac1f4b
segin2005 <segin2005@8792b224-6933-e0e1-cdff-19ba7c209891>
2011-05-29 09:57:51 +0000

    Added updated copyright notice

--- kshell/kshell.c
+++ kshell/kshell.c
@@ -5,7 +5,19 @@
  * tty... Well, really, it just kills the program's stdin/stdout/stderr before
  * it even launches the program.
  *  
- * Copyright (C) 2006 Segin
+ * Copyright (c) 2006, 2011, Kirn Gill <segin2005@gmail.com>
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  * 
  * Version 1.1, 2006 May 15
  */
