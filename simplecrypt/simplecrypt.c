a2a6445f45968cf44d576dcc605cb727f85762a6
segin2005@gmail.com <segin2005@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-02-17 11:05:24 +0000

    Historical import of simplecrypt

--- simplecrypt/simplecrypt.c
+++ simplecrypt/simplecrypt.c
@@ -1,20 +1,22 @@
 /* Simplecrypt - Simple Gmail file obfuscator. Public domain.
  * Use for executable attachments or archives containing executables.
  * Written in 2012 by Kirn Gill II <segin2005@gmail.com>
+ * Idea by Mattis Michel <sic_zer0@hotmail.com>
  */
 
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	FILE *fd, fd2;
-	if(!(fd = fopen(argv[1], "rb") || !(fd2 = fopen(argv[2], "wb") {
+	FILE *fd, *fd2;
+	char a;
+	if(!(fd = fopen(argv[1], "rb")) || !(fd2 = fopen(argv[2], "wb"))) {
 		if(fd)
 			perror(argv[2]);
 		else
 			perror(argv[1]);
-		exit;
+		exit(1);
 	}
-	char a;
 	while(fread(&a, 1, 1, fd)) {
 		a ^= 0xFF;
 		fwrite(&a, 1, 1, fd2);
