d628a103715fd7d26d5a395b74a453fbd1bb767e
supermaties@gmail.com <supermaties@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-02-19 07:05:53 +0000

    moved getargs() and all it's base to misc.c

--- matpic/misc.c
+++ matpic/misc.c
@@ -2,6 +2,7 @@
 #include "as.h"
 #include "dis.h"
 #include "main.h"
+#include "str.h" /* skipsp(), getnum(), getid(), cmpid(), alfa[] */
 
 void cleanup(void) {
 	arr_free(&inss);
@@ -36,5 +37,171 @@ void flwarn(char *file, int line, char *msg) {
 
 void fawarn(char *file, int addr, char *msg) {
 	fprintf(stderr, "%s: %X: %s\n", file, addr, msg);
+}
+
+unsigned int getval(char **src) {
+	unsigned int val;
+	int not = 0;
+
+	skipsp(src);
+	while (**src == '!' || **src == '~')
+		++not, ++*src;
+	skipsp(src);
+	if (**src == '(') {
+		++*src;
+		val = numarg(src);
+		if (**src != ')')
+			aerrexit("missing ')'");
+		++*src;
+	} else if (**src == '$') {
+		val = address;
+		++*src;
+	} else if(!getnum(src, &val)) {
+		int i;
+		label_t *label;
+		char *id;
+
+		id = getid(src);
+		if (id == NULL)
+			goto gotval;
+
+		/* get label or fail */
+		for (i = 0; i < labels.count; ++i) {
+			label = (label_t *) ((label_t *) labels.data) + i;
+			if (cmpid(label->name, id)) {
+				val = label->address;
+				goto gotval;
+			}
+		}
+		aerrexit("unknown identifier");
+	}
+	gotval:
+	if (not)
+		val = ~val;
+	return val;
+}
+
+enum ops {
+	OP_ADD,
+	OP_SUB,
+	OP_MUL,
+	OP_DIV,
+	OP_MOD,
+	OP_AND,
+	OP_IOR,
+	OP_EOR,
+	OP_SHL,
+	OP_SHR
+};
+
+unsigned int numarg(char **src) {
+	unsigned int lval, rval, op;
+
+	lval = getval(src);
+
+	while(1) {
+		skipsp(src);
+		switch (**src) {
+			case '+':
+				op = OP_ADD;
+				++*src;
+				break;
+			case '-':
+				op = OP_SUB;
+				++*src;
+				break;
+			case '*':
+				op = OP_MUL;
+				++*src;
+				break;
+			case '/':
+				op = OP_DIV;
+				++*src;
+				break;
+			case '%':
+				op = OP_MOD;
+				++*src;
+				break;
+			case '&':
+				op = OP_AND;
+				++*src;
+				break;
+			case '|':
+				op = OP_IOR;
+				++*src;
+				break;
+			case '^':
+				op = OP_EOR;
+				++*src;
+				break;
+			case '<':
+				if (*((*src) + 1) == '<') {
+					op = OP_SHL;
+					*src += 2;
+					break;
+				}
+				return lval;
+			case '>':
+				if (*((*src) + 1) == '>') {
+					op = OP_SHR;
+					*src += 2;
+					break;
+				}
+				return lval;
+			default:
+				return lval;
+		}
+
+		rval = getval(src);
+
+		switch (op) {
+			case OP_ADD:
+				lval += rval;
+				break;
+			case OP_SUB:
+				lval -= rval;
+				break;
+			case OP_MUL:
+				lval *= rval;
+				break;
+			case OP_DIV:
+				lval /= rval;
+				break;
+			case OP_MOD:
+				lval %= rval;
+				break;
+			case OP_AND:
+				lval &= rval;
+				break;
+			case OP_IOR:
+				lval |= rval;
+				break;
+			case OP_EOR:
+				lval ^= rval;
+				break;
+			case OP_SHL:
+				lval <<= rval;
+				break;
+			case OP_SHR:
+				lval >>= rval;
+		}
+	}
+}
+
+int getargs(char **src, int *args) {
+	int n = 0;
+
+	if (!*src)
+		return 0;
+	while (1) {
+		args[n] = numarg(src);
+		if (alfa[(unsigned char) **src] & (CT_NUL | CT_NL))
+			return n + 1;
+		if (**src != ',')
+			aerrexit("your argument is invalid");
+		++n, ++*src;
+		if (n == ARG_MAX)
+			aerrexit("too many arguments");
+	}
 }
 
--- matpic/misc.h
+++ matpic/misc.h
@@ -1,12 +1,12 @@
 #ifndef __MISC_H__
 #define __MISC_H__
 
-extern void cleanup(void);
-
+void cleanup(void);
 void errexit(char *msg);
 void flerrexit(char *file, int line, char *msg);
 void flwarn(char *file, int line, char *msg);
 void fawarn(char *file, int addr, char *msg);
+int getargs(char **src, int *args);
 
 #endif /* __MISC_H__ */
 
