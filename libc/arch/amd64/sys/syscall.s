3a2cc1c4f06392554279ba76a386af3247c9c826
segin2005@gmail.com <segin2005@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-03-08 02:32:11 +0000

    Replaced failed attempt at amd64 syscall.s with proper matlibc version, AMD64 SysV ABI conforming.

--- libc/arch/amd64/sys/syscall.s
+++ libc/arch/amd64/sys/syscall.s
@@ -1,19 +1,26 @@
-
-# entrypoint for syscall
-
 .text
-	.align 16
-
+.align 16, 0x90
 .globl syscall
-	.type syscall,@function
-	
+.type syscall, @function
 syscall:
-	pop %rcx
-	pop %rax
-	movq %rcx,%r10
-	movq %rsp(-16),%rdi
-	movq %rsp(-24),%rsi
-	movq %rsp(-32),%
+	movq %rdi, %rax
+	movq %rsi, %rdi
+	movq %rdx, %rsi
+	movq %rcx, %rdx
+	movq %r8, %r10
+	mov %r9, %r8
+	mov 8(%rsp), %r9 /* argument 6 is on stack */
 	syscall
-	push %rcx
-	ret		
+	cmp $-4095, %rax
+	jnc err
+	ret
+err:
+	negq %rax
+#ifdef PIC
+	movq errno@GOTPCREL(%rip), %rcx
+	movl %eax, (%rcx)
+#else
+	movl %eax, errno(%rip)
+#endif /* PIC */
+	movq $-1,%rax
+	ret
