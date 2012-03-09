607d47a32a9fbded60aec2c67b8e6a381a2f6f95
segin2005@gmail.com <segin2005@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-03-09 05:32:01 +0000

    Imported from matlibc

---
+++ libc/include/errno.h
@@ -0,0 +1,10 @@
+/* errno.h: C standard error number global variable
+ * Written by Mattis Michel <sic_zer0@hotmail.com>
+ */
+
+#ifndef __ERRNO_H__
+#define __ERRNO_H__
+
+extern int errno;
+
+#endif /* __ERRNO_H__ */
---
+++ libc/include/types.h
@@ -0,0 +1,10 @@
+/* types.h: Legacy wrapper for sys/types.h
+ * Written by Mattis Michel <sic_zer0@hotmail.com>
+ */
+
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+#include <sys/types.h>
+
+#endif /* __TYPES_H__ */
