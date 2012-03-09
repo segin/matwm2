e626f8c25edd17613df414b9e6845a48dc09dbce
segin2005@gmail.com <segin2005@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-03-09 05:42:47 +0000

    Added prototypes/defines from matlibc's unistd.h

--- libc/include/unistd.h
+++ libc/include/unistd.h
@@ -7,11 +7,19 @@
 
 #include <sys/types.h>
 
+#define STDIN_FILENO 0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+
 extern	void	_exit(int);	
 extern	pid_t	fork(void);
 extern	pid_t	vfork(void);
 
 extern	ssize_t	read(int, void *, size_t);
 extern	ssize_t	write(int, void *, size_t);
+
+extern off_t lseek(int fd, off_t off, int whence);
+extern int brk(void *addr);
+
 
 #endif /* __UNISTD_H__ */
