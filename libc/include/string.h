5b6a012074ffec822404592b14e9cbaf9d207aaa
segin2005@gmail.com <segin2005@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-03-09 05:50:21 +0000

    I surrender, adding OpenBSD's libc's string crap, same as matlibc. Also importing Makefile a la matlibc.

--- libc/include/string.h
+++ libc/include/string.h
@@ -1,8 +1,103 @@
-/* string.h: C standard string functions 
- * Written by Kirn Gill <segin2005@gmail.com>
+/*	$OpenBSD: string.h,v 1.23 2012/01/17 02:48:01 guenther Exp $	*/
+/*	$NetBSD: string.h,v 1.6 1994/10/26 00:56:30 cgd Exp $	*/
+
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)string.h	5.10 (Berkeley) 3/9/91
  */
- 
+
 #ifndef __STRING_H__
 #define __STRING_H__
+
+#include <types.h> /* size_t, ssize_t */
+#include <stdlib.h> /* NULL */
+
+extern void *memchr(const void *, int, size_t);
+extern void *memrchr(const void *, int, size_t);
+extern int memcmp(const void *, const void *, size_t);
+extern void *memcpy(void *, const void *, size_t);
+extern void *memmove(void *, const void *, size_t);
+extern void *memset(void *, int, size_t);
+extern char *strcat(char *, const char *);
+extern char *strchr(const char *, int);
+extern int strcmp(const char *, const char *);
+extern int strcoll(const char *, const char *);
+extern char *strcpy(char *, const char *);
+extern size_t strcspn(const char *, const char *);
+extern char *strerror(int);
+extern size_t strlen(const char *);
+extern char *strncat(char *, const char *, size_t);
+extern int strncmp(const char *, const char *, size_t);
+extern char *strncpy(char *, const char *, size_t);
+extern char *strpbrk(const char *, const char *);
+extern char *strrchr(const char *, int);
+extern size_t strspn(const char *, const char *);
+extern char *strstr(const char *, const char *);
+extern char *strtok(char *, const char *);
+extern char *strtok_r(char *, const char *, char **);
+extern size_t strxfrm(char *, const char *, size_t);
+
+#if __BSD_VISIBLE || __XPG_VISIBLE
+extern void *memccpy(void *, const void *, int, size_t);
+#endif
+
+#if __BSD_VISIBLE || __XPG_VISIBLE >= 420
+extern int bcmp(const void *, const void *, size_t);
+extern void bcopy(const void *, void *, size_t);
+extern void bzero(void *, size_t);
+extern int ffs(int);
+extern char *index(const char *, int);
+extern char *rindex(const char *, int);
+extern int strcasecmp(const char *, const char *);
+extern int strncasecmp(const char *, const char *, size_t);
+extern char *strdup(const char *);
+#endif
+
+#if __BSD_VISIBLE || __XPG_VISIBLE >= 600
+int	 strerror_r(int, char *, size_t);
+#endif
+
+#if __POSIX_VISIBLE >= 200809
+extern char *stpcpy(char *, const char *);
+extern char *stpncpy(char *, const char *, size_t);
+extern char *strndup(const char *, size_t);
+extern size_t strnlen(const char *, size_t);
+#endif
+
+#if __BSD_VISIBLE
+extern char *strcasestr(const char *, const char *);
+size_t strlcat(char *, const char *, size_t);
+extern size_t strlcpy(char *, const char *, size_t);
+void strmode(int, char *);
+extern char *strsep(char **, const char *);
+extern char *strsignal(int);
+extern int timingsafe_bcmp(const void *, const void *, size_t);
+#endif 
 
 #endif /* __STRING_H__ */
