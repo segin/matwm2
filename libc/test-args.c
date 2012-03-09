3581b12b6a3796ee865a343d00c73447b0ad113e
segin2005@gmail.com <segin2005@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-03-09 08:03:23 +0000

    Added __progname test to testcase as well

--- libc/test-args.c
+++ libc/test-args.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <fcntl.h>
 
+extern char* __progname;
+
 int fdprint(int fd, char *str) {
 	return write(fd, str, strlen(str));
 }
@@ -13,6 +15,9 @@ int print(char *str) {
 
 int main(int argc, char *argv[]) {
 	int i;
+	print("progname: ");
+	print(__progname);
+	print("\n");
 	for (i = 0; i < argc; ++i) {
 		print(argv[i]);
 		print(" ");
