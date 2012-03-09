399dd594abaf6cfc2ce4bd3394d31423f7506112
segin2005@gmail.com <segin2005@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-03-09 07:54:41 +0000

    testing new crt0 prototype

--- libc/crt0.c
+++ libc/crt0.c
@@ -13,9 +13,17 @@ char** environ;
 
 extern int main(int argc, char **argv, char **envp);
 
-void _start(int argc, char **argv, char **envp)
+void _start(char **ap, void (*cleanup)(void), void *unused, void *unused2)
 {
+	int argc;
+	char **argv;
+	char **envp;
+	
+	argc = * (long *) ap;
+	argv = ap + 1;
+	envp = ap + 2 + argc;
+	
 	environ = envp;
-	argv[0] ? __progname = argv[0] : __progname = "";
+	*argv[0] ? __progname = argv[0] : __progname = "";
 	exit(main(argc, argv, envp));
 }
