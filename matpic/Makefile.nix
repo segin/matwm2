5a20390300112ee4e63927ac88bd78120983e30d
supermaties@gmail.com <supermaties@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-03-19 22:07:04 +0000

    install target

--- matpic/Makefile.nix
+++ matpic/Makefile.nix
@@ -1,5 +1,8 @@
-CFLAGS += -g -Wall -ansi -pedantic
+PREFIX  = /usr/local
+INSTALL = install
+RM      = rm -f
 
+CFLAGS += -g -Wall -ansi -pedantic
 OBJS = str.o mem.o arch.o pic14b.o as.o main.o misc.o ihex.o dis.o ppc.o io.o lineno.o
 
 all: matpic
@@ -9,3 +12,9 @@ matpic: $(OBJS)
 
 clean:
 	$(RM) $(OBJS) matpic
+
+install: matpic
+	$(INSTALL) -s matpic $(PREFIX)/bin/matpic
+
+deinstall:
+	$(RM) $(PREFIX)/bin/matpic
