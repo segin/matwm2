57f3bc0fb9b1220429d4ed04398da44b6556e576
segin2005@gmail.com <segin2005@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-04-08 17:43:01 +0000

    More skeletal structure to emulator

--- dcpu16/dcpu.c
+++ dcpu16/dcpu.c
@@ -16,7 +16,21 @@ int main(int argc, char *argv[])
 		 reg_j, reg_pc, reg_sp, reg_o;
 
 	/* main system memory */
-	char mem[0x10000];
+	uint16_t mem[0x10000];
+	
+	/* CPU internals */
+	uint16_t op_code, op_a, op_b;
+
+	/* fuck you, goto is perfectly fine if used correctly */
+init:
+	reg_a = reg_b = reg_c = reg_x = reg_y = reg_z = reg_i = reg_j = reg_pc 
+        = reg_o = 0;
+	reg_sp = 0xffff;
+
+	/* CPU main loop */
+	while(1) { 
+	
 
 
+	}
 }
