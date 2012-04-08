c886437f3abca4077fdebadec6a946072522e838
segin2005@gmail.com <segin2005@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-04-08 16:31:02 +0000

    Start of emulator code

---
+++ dcpu16/dcpu.c
@@ -0,0 +1,20 @@
+/* DCPU-16 emulator, as seen on 0x10c 
+ * ISA specification is copyright 2012, Mojang Ab
+ * This emulator is copyright Kirn Gill <segin2005@gmail.com>
+ * See COPYING for license text.
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[]) 
+{
+
+	/* the registers */ 
+	uint16_t reg_a, reg_b, reg_c, reg_x, reg_y, reg_z, reg_i,
+		 reg_j, reg_pc, reg_sp, reg_o;
+
+
+
+}
