de04173ef56ee8f778e046ed7eee31b335b295b2
segin2005@gmail.com <segin2005@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-04-08 16:34:17 +0000

    Checkpoint.

--- dcpu16/dcpu.c
+++ dcpu16/dcpu.c
@@ -1,6 +1,6 @@
 /* DCPU-16 emulator, as seen on 0x10c 
  * ISA specification is copyright 2012, Mojang Ab
- * This emulator is copyright Kirn Gill <segin2005@gmail.com>
+ * This emulator is copyrighti 2012 Kirn Gill <segin2005@gmail.com>
  * See COPYING for license text.
  */
 
@@ -15,6 +15,8 @@ int main(int argc, char *argv[])
 	uint16_t reg_a, reg_b, reg_c, reg_x, reg_y, reg_z, reg_i,
 		 reg_j, reg_pc, reg_sp, reg_o;
 
+	/* main system memory */
+	char mem[0x10000];
 
 
 }
