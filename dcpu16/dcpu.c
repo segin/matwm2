6109a0a5804c688f0868b2ebf65dec48506d6533
segin2005@gmail.com <segin2005@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-04-09 08:27:53 +0000

    Added enumeration symbolics for basic opcodes, non-basic opcodes, and all non-literal operands. Added a little more code for the actual CPU itself.

--- dcpu16/dcpu.c
+++ dcpu16/dcpu.c
@@ -1,6 +1,7 @@
-/* DCPU-16 emulator, as seen on 0x10c 
- * ISA specification is copyright 2012, Mojang Ab
- * This emulator is copyrighti 2012 Kirn Gill <segin2005@gmail.com>
+/* Emulator for the DCPU-16, as featured in the game 0x10c
+ * ISA specification is copyright 2012, Mojang Ab. Based on ISA spec v1.1
+ * See 0x10c.com for details / updated ISA specifications.
+ * This emulator is copyright 2012 Kirn Gill <segin2005@gmail.com>
  * See COPYING for license text.
  */
 
@@ -8,9 +9,68 @@
 #include <stdio.h>
 #include <sys/types.h>
 
+enum basicops { 
+	OP_NONBASIC = 0, /* extended opcode */
+	OP_SET, /* sorta like MOV in most archs */
+	OP_ADD, /* addition */
+	OP_SUB, /* subtraction */
+	OP_MUL, /* multiplicaton */
+	OP_DIV, /* division */
+	OP_MOD, /* modulus */
+	OP_SHL, /* barrel-shift left */
+	OP_SHR, /* barrel-shift right */
+	OP_AND, /* binary and */
+	OP_BOR, /* BORing operator, I kid, binary or */
+	OP_XOR, /* exclusive or, used by shellcode to make zeros */
+	/* The following are WTF instructions */
+	OP_IFE, /* execute following op if operands are equal */
+	OP_IFN, /* execute following op if operands aren't equal */
+	OP_IFG, /* execute following op if first operand greater than second */
+	/* and finally, the most WTF of the basic instructions */
+	OP_IFB  /* execute following op if binary and of operands is not zero */
+};
+
+enum nonbasicops { 
+	XOP_JSR = 1 /* jump and save return */ 
+};
+
+enum operands {
+	IMM_A = 0, /* register immediates */
+	IMM_B,
+	IMM_C,
+	IMM_X,
+	IMM_Y, 
+	IMM_Z,
+	IMM_I,
+	IMM_J,
+	PTR_A, /* register is a pointer */
+	PTR_B,
+	PTR_C,
+	PTR_X,
+	PTR_Y,
+	PTR_Z,
+	NWP_A, /* pointer is (PC + 1) + register ? */
+	NWP_B,
+	NWP_C,
+	NWP_X,
+	NWP_Y,
+	NWP_Z,
+	NWP_I,
+	NWP_J,
+	/* Can these be used opposite of their normal intended use?*/
+	POP,   /* read at stack pointer, increment stack pointer */
+	PEEK,  /* read (write?) at stack pointer */
+	PUSH,  /* write to stack pointer, decrement stack pointer */
+	IMM_SP, /* stack pointer as a register immediate */
+	IMM_PC, /* program counter as a register immediate */
+	IMM_O, /* Overflow indicator as a register immediate */
+	NWP,   /* pointer at PC + 1 */
+	IMP   /* literal at PC + 1 */
+	/* Everything from here to 0x3f is an immediate literal - 0x20 */
+};
+
 int main(int argc, char *argv[]) 
 {
-
 	/* the registers */ 
 	uint16_t reg_a, reg_b, reg_c, reg_x, reg_y, reg_z, reg_i,
 		 reg_j, reg_pc, reg_sp, reg_o;
@@ -19,7 +79,8 @@ int main(int argc, char *argv[])
 	uint16_t mem[0x10000];
 	
 	/* CPU internals */
-	uint16_t op_code, op_a, op_b, tmp_a, tmp_b;
+	uint16_t op_code, op_a, op_b, op_ac, op_bc, tmp_a, tmp_b, o;
+	uint32_t tmp_ax, tmp_bx;
 
 	/* And finally, a cycle counter for benchmarking */
 	uint64_t cycles;
@@ -33,8 +94,22 @@ init:
 	/* CPU main loop */
 	while(1) { 
 		op_code = mem[reg_pc];
+		op_ac = (op_code >> 4) & 0x3f;
+		op_bc = (op_code >> 10) & 0x3f;
+		op_code =& 0xf;
+		
+		if(!op_code) goto non_basic;
+
+decode_operand: 		
+		switch(o ? op_ac : op_bc) { 
+
 		
 
-
+non_basic: 		
+		switch (op_a) { 
+			case XOP_JSR:
+			/* do shit */
+			break;
+		}
 	}
 }
