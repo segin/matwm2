c133c1540560d81ddcd149c966027bf7ea74ece1
supermaties@gmail.com <supermaties@gmail.com@8792b224-6933-e0e1-cdff-19ba7c209891>
2012-05-11 01:29:47 +0000

    addition of matkermit, which is an attempt at a minimal kermit client that should suffice to communicate with hp-4x and hp-50g calculators

---
+++ matkermit/kermit.c
@@ -0,0 +1,31 @@
+#include "kpacket.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+char kpacket[KPACKET_MAXLEN];
+int kfd, seq;
+
+int kpacket_send(int type, char *data, int len) {
+	len = kpacket_fill(kpacket, seq, type, data, len);
+	return write(kfd, kpacket, len);
+}
+
+int main(int argc, char *argv[]) {
+	int fd, l;
+	fd = open("/dev/ttyUSB0", O_RDWR); /* TODO let user specify & error check */
+	l = kpacket_fill(kpacket, 0, KPACKET_TYPE_GEN, "D", 1);
+	write(fd, kpacket, l);
+	read(fd, kpacket, 4);
+	fprintf(stderr, "%c\n", kpacket[3]);
+	l = kpacket_fill(kpacket, kpacket[2], KPACKET_TYPE_ACK, "", 0);
+	write(fd, kpacket, l);
+	while (kpacket[0] != 01)
+		read(fd, kpacket, 1);
+	read(fd, kpacket, 4);
+	fprintf(stderr, "%c\n", kpacket[2]);
+	return 0;
+}
+
---
+++ matkermit/kpacket.h
@@ -0,0 +1,55 @@
+#ifndef __KPACKET_H__
+#define __KPACKET_H__
+
+/* Absolute maximum length for kermit packet (with space for a 0) */
+/* 4 bytes header + max data (94) + 1 byte checksum + \r\n + NUL = 102 */
+#define KPACKET_MAXLEN  102
+
+/* Response types */
+#define KPACKET_TYPE_DATA 'D' /* Data packet */
+#define KPACKET_TYPE_ACK  'Y' /* Acknowledgement */
+#define KPACKET_TYPE_NAK  'N' /* Negative acknowledgement */
+#define KPACKET_TYPE_SEND 'S' /* Send initiative (exchange parameters) */
+#define KPACKET_TYPE_EOT  'E' /* Break transmission */
+#define KPACKET_TYPE_FILE 'F' /* File header */
+#define KPACKET_TYPE_EOF  'Z' /* End of file */
+#define KPACKET_TYPE_ERR  'E' /* Error */
+#define KPACKET_TYPE_ATTR 'A' /* Optional attribute packet */
+/* note that Q and T are reserved */
+
+/* Server commands */
+#define KPACKET_TYPE_RECV 'R' /* Ask for receiving a file */
+#define KPACKET_TYPE_INIT 'I' /* Initialize (exchange parameters) */
+#define KPACKET_TYPE_TEXT 'X' /* Text header (response of command) */
+#define KPACKET_TYPE_CMD  'C' /* Run host command */
+#define KPACKET_TYPE_KERM 'K' /* Run kermit command */
+#define KPACKET_TYPE_GEN  'G' /* Generic kermit command */
+/* Generic kermit commands
+ *  in comments before - operands are described
+ *  Optional operands are in []
+ *  Operands are prefixed with 1 byte specifying their length
+ *  With arguments in the form [a][b] i believe a needs length 0 if not given
+ */
+#define KPACKET_SCMD_LOGI 'I' /* [user[pass[acct]]] - login */
+#define KPACKET_SCMD_CWD  'C' /* [directory[password]] - change directory */
+#define KPACKET_SCMD_LOGO 'L' /* - logout */
+/* these are optional */
+#define KPACKET_SCMD_FINI 'F' /* - finish (shutdown the server but no logout) */
+#define KPACKET_SCMD_DIR  'D' /* [filespec] - query directory listing */
+#define KPACKET_SCMD_DUSG 'U' /* [area] - disk usage query */
+#define KPACKET_SCMD_DEL  'E' /* filespec - delete */
+#define KPACKET_SCMD_TYPE 'T' /* filespec - type */
+#define KPACKET_SCMD_REN  'R' /* oldname,newname - rename */
+#define KPACKET_SCMD_COPY 'K' /* source,destination - copy */
+#define KPACKET_SCMD_WHO  'W' /* [uid_or_host[options]]- who is logged in */
+#define KPACKET_SCMD_MSG  'M' /* destination,text - send short message */
+#define KPACKET_SCMD_HELP 'H' /* [topic] - ask for help */
+#define KPACKET_SCMD_STAT 'Q' /* - query server status */
+#define KPACKET_SCMD_PROG 'P' /* [filespec][commands] - program */
+#define KPACKET_SCMD_JOUR 'J' /* command[argument] - journal */
+#define KPACKET_SCMD_VAR  'V' /* command[argument[argument]] - variable */
+
+int kpacket_fill(char *packet, int seq, int type, char *data, int len);
+
+#endif /* __KPACKET_H__ */
+
